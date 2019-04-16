b2cc41f2713c3358052788538945cf18700e3d10
Igor Santos <igornzds@gmail.com>
2019-04-16 11:04:35 -0300

    Correct styles

--- _js/script.js
+++
@@ -1,8 +0,0 @@
-const icon = document.querySelector('.nav-menu-hamburguer');
-const menu = document.querySelector('.menu-hambuguer');
-
-const aparecerMenuTop = function(){
-    menu.classList.toggle('mostrar');
-}
-
-icon.onclick = aparecerMenuTop;
